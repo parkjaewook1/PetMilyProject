e57a9dfe778d6f71d16d11fa707bd7fedc208a7c
parkjaewook1 <wodnr5050@gmail.com>
2025-11-24 17:59:47 +0900

    diaryComment.java
    private String profileImage; 추가

--- backend/src/main/java/com/backend/domain/diary/DiaryComment.java
+++ backend/src/main/java/com/backend/domain/diary/DiaryComment.java
@@ -15,7 +15,8 @@ public class DiaryComment {
     private Integer replyCommentId;
     private String nickname;
     private LocalDateTime inserted;
-
+    
+    private String profileImage;
     // ✅ 대댓글 관련 추가
     private List<DiaryComment> replies; // 자식 대댓글 목록 (미리보기 or 전체)
     private Integer replyCount;         // 대댓글 총 개수
